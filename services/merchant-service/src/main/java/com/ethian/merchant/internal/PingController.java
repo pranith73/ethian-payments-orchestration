fd604b2c2495c47bc2d9bb38d04826048bff1528
Pranith <pranith0147@gmail.com>
2025-12-12 12:17:57 +0530

    feat(merchant-service): add internal ping endpoint

---
+++ services/merchant-service/src/main/java/com/ethian/merchant/internal/PingController.java
@@ -0,0 +1,14 @@
+package com.ethian.merchant.internal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/internal/ping")
+    public String ping() {
+        return "merchant-service: OK";
+    }
+}
+
